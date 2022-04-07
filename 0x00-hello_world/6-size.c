84dde85509a20ee198d0cfff1ada24a32efecc4b
Moha Luche <mohaluche40@gmail.com>
2022-04-08 02:42:39 +0300

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ long int b;
 long long int c;
 char d;
 float f;
+
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
