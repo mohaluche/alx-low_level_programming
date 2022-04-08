7f3e1a7d1f40a3366fde9b1f2ff49ee69d649d74
Moha Luche <mohaluche40@gmail.com>
2022-04-08 21:39:48 +0300

    My betty style task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,23 +10,24 @@
 
 int main(void)
 {
-int n:
+int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
-{printf("%i is zero\n", n);
+printf("%i is zero\n", n);
 }
 
 else if (n < 0)
-{printf("%i is negetive\n", n);
+{
+printf("%i is negetive\n", n);
 }
 
 else
 {
-printf("%i is positive\n", n)
+printf("%i is positive\n", n);
 }
 
 return (0);
