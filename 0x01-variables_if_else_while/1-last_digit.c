d56d06694070be1427c691bffe25ad46e3c8c39c
Moha Luche <mohaluche40@gmail.com>
2022-04-08 23:12:23 +0300

    My last digit task commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,14 +21,15 @@ if (n % 10 > 5)
 printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
 
-else (n % 10 == 0);
+else if (n % 10 == 0);
 {printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
 
-else if
+else
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
+
 
 return(0);
 }
