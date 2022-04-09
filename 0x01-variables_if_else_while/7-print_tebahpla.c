8a35ebeb3e4db6484e8eb901951f034726f16aad
Moha Luche <mohaluche40@gmail.com>
2022-04-09 17:15:38 +0300

    My commit message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry poiny
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+	int main(void)
+{
+	char alphabet;
+	for (alphabet = 122 ; alphabet >= 97; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+}
