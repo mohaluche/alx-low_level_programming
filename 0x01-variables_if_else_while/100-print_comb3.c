14e3753512d98829e1716d26ba0aba69ad1b5347
Moha Luche <mohaluche40@gmail.com>
2022-04-10 00:32:13 +0300

    My commit message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,7 @@
  */
 int main(void)
 {
-	int digit1, digit2;
-	
+	int digit1, digit2;	
 	for (digit1 = 0; digit2 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -18,7 +17,7 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-		 	continue;
+			continue;
 
 			putchar(',');
 			putchar(' ');
