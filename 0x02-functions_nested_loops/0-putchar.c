214796eb86584def7167eeccde6ae7e8a2650e33
Moha Luche <mohaluche40@gmail.com>
2022-04-13 00:10:20 +0300

    A message commit for program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - enty point
+ * A program that prints _putchar
+ * return: 0 Success
+ */
+int main()
+{
+	printf("_putchar\n");
+
+		return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!bin/bash
+My read me file for 0x02. C - Functions and nested loops project.
