c95ee44f2e24c33fda14d9ac94ab769b1f4a2ac9
Moha Luche <mohaluche40@gmail.com>
2022-04-13 01:23:34 +0300

    Commit for print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * A function that prints the alphabet, in lowercase.
+ * 
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c = 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
