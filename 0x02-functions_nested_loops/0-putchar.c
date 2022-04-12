62715f5ace09a5db5e28c75844aa0e9a5c89a721
Moha Luche <mohaluche40@gmail.com>
2022-04-13 01:02:13 +0300

    A message commit for program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - enty point
- * A program that prints _putchar
- * return: 0 Success
+ * main-main block
+ * Description:A program that prints _putchar
+ * Return: 0 Success
  */
 int main(void)
 {
