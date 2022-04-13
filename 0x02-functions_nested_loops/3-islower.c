91a09abc2e6687a70712448d3a0d62db01f531b8
Moha Luche <mohaluche40@gmail.com>
2022-04-13 14:56:56 +0300

    Commit for return 1 if lowercase and 0 otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if lowercase and 0 otherwise
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+int _slower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
