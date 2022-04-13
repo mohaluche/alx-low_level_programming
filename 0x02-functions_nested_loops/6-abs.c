69a504c9b3171700ddd07eeebda8105036936a01
Moha Luche <mohaluche40@gmail.com>
2022-04-14 00:01:58 +0300

    A commit for identifying whether 0, 1 or -1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - function that prints a ablolute value of number
+ * @n: takes in an integer
+ * Return: 1 if greater than 0, 0 if zero and -1 if less than 0
+ * */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
