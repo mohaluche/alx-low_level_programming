6245fcdc08ccb1ff1015ad9f2b7c9068496f5947
Moha Luche <mohaluche40@gmail.com>
2022-04-13 21:41:46 +0300

    Commit for return 1 if lowercase and 0 otherwise

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * Printing lowercase alphabets 10 times 
+ * Printing lowercase alphabets 10 times
+ * Return: 0 
  **/
 void print_alphabet_x10(void)
 {
-	int count = 0;
-		char letter;
+	int i;
+	char c;
 
-	while (count++ <= 9);
+	for (i = 0; i<10; i++);
 	{
-		for (letter = 'a'; letter <= 'z'; letter++);
-	_putchar(letter);
+		for (c = 'a'; c <= 'z'; c++);
+
+		_putchar(c);
 		_putchar('\n');
 		}
 }
