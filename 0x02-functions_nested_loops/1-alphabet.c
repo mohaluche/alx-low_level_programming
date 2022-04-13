70960042b0beb877cda16a004713e981cf07d52b
Moha Luche <mohaluche40@gmail.com>
2022-04-13 14:32:34 +0300

    A commit for the function that prints 10 times alphabet followed by new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * main - main entry
  * A function that prints the alphabet, in lowercase.
- * Return: 0
  **/
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-
 		_putchar(c);
-
 	_putchar('\n');
 }
