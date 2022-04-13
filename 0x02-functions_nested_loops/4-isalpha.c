c2f0c3cf5460fb702deb28868aacbe54f984117d
Moha Luche <mohaluche40@gmail.com>
2022-04-13 22:59:00 +0300

    Returns 1 if character is latter,  lowercas or uppercase and otherwise 0.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,7 @@
  * Returns: 1 if c is letter, lowercase or uppercase otherwise 0
  * @c: takes in a character
  **/
-int _isalpha(int c)
-
-{
+int _isalpha(int c) {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
 		return (1);
