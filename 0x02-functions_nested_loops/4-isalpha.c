4ef1ba607385829bbeef24dbeb148f85bc0efa03
Moha Luche <mohaluche40@gmail.com>
2022-04-13 23:03:59 +0300

    Returns 1 if character is latter,  lowercas or uppercase and otherwise 0.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * Returns: 1 if c is letter, lowercase or uppercase otherwise 0
  * @c: takes in a character
+ * Returns: 1 if c is letter, lowercase or uppercase otherwise 0
  **/
-int _isalpha(int c) {
+int _isalpha(int c)
+{
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
 		return (1);
