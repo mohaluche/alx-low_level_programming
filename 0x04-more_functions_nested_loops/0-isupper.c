17c86eba171fb9bdd82117ff2084893df7765f47
Moha Luche <mohaluche40@gmail.com>
2022-04-14 23:29:35 +0300

    A commit for function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * _isuppur - checks upper case
+ * @: print int
+ * Return: gives 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
