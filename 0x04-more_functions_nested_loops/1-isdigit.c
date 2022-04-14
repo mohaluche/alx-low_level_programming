1f515b2cb9857d0ce1fb49f9d233856c138ef2b5
Moha Luche <mohaluche40@gmail.com>
2022-04-15 00:21:04 +0300

    A commit for evaluation of digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,18 +2,19 @@
 /**
  * _isdigit - if is a digit 
  * @c: print int
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 57)
-	{
-		return (1);
-	}
+if (c >= 0 && c <= 57)
+{
+return (1);
+}
 
-	else
-	{
-		return (0);
-	}
+else
+{
+return (0);
+}
+
 }
