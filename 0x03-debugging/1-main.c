8c36e56b7065db6615f2c71b8ab3d780e40eb6fb
Moha Luche <mohaluche40@gmail.com>
2022-04-14 15:26:37 +0300

    A message commit for a code that prints out the largest of three numbers

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,17 +9,18 @@ int main(void)
 {
 	int i;
 
-	 printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-	 i = 0;
+	i = 0;
 
-	 /**
-	  * while (i < 10)
-	  * {
-	  * Infinite loop - No increase of variable
-	  * putchar(i);
-	  * }
-	  */
-	 printf("Infinite loop avoided! \\o/\n");
-	 return (0);
+       	/**
+	* while (i < 10)
+	* {
+	* Infinite loop - No increase of variable
+	* putchar(i);
+	* }
+	*/
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
