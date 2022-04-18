5e26289b718e97fa4973335a399ec535e902fa4d
Moha Luche <mohaluche40@gmail.com>
2022-04-18 22:56:19 +0300

    Task 0 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code 
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    int n;
+       	int n;
 
-	        n = 98;
-		_putchar("n=%d\n", n);
-		    reset_to_98(&n);
-		        _putchar("n=%d\n", n);
-			    return (0);
+	n = 98;
+	putchar("n=%d\n", n);
+	reset_to_98(&n);
+	putchar("n=%d\n", n);
+	return (0);
 }
