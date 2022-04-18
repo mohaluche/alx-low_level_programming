fbedddecd9c89fc1cb5c7df6bab5d7e4bb7056bc
Moha Luche <mohaluche40@gmail.com>
2022-04-18 22:53:01 +0300

    Task 0 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,8 +11,8 @@ int main(void)
 	    int n;
 
 	        n = 98;
-		printf("n=%d\n", n);
+		_putchar("n=%d\n", n);
 		    reset_to_98(&n);
-		        printf("n=%d\n", n);
+		        _putchar("n=%d\n", n);
 			    return (0);
 }
