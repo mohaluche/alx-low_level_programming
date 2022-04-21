92be8ba883d021386efb4c350bfc80670a37c0f1
Moha Luche <mohaluche40@gmail.com>
2022-04-21 21:41:13 +0300

    function that copies a strin

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,28 @@
- 
- #include "main.h" 
- /** 
- * _strncpy - copie the string 
- * @dest: array that returns 
- * @src: the pointer that receive the string 
- * @n: times to interate 
- * 
- * Return: the parameter dest. 
+ #include "main.h"
+ /**
+ * _strncpy - copie the string
+ * @dest: array that returns
+ * @src: the pointer that receive the string
+ * @n: times to interate
+ *
+ * Return: the parameter dest.
  */
-char*_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	 int i;
 
 i = 0;
-while(i < n && src[i] != '\0') 
+while(i < n && src[i] != '\0')
  {
 	 dest[i] = src[i];
- i++;
+	 i++;
  }
 
  i = i;
- while(i < n)
+ while (i < n)
  {
 	 dest[i] = '\0';
- i++; 
- } 
- return(dest); 
+	 i++;
+ }
+ return(dest);
  }
